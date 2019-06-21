16c376c1ab201a44d3a03515dc5200bdba6c6749
Stephan Poetschner <stephan.poetschner@gmail.com>
2019-06-21 21:42:04 +0200

    + updating bootstrap css in project

--- portfolio/static/js/main.js
+++ portfolio/static/js/main.js
@@ -1,18 +1,5 @@
 (function ($) {
   $(document).ready(function () {
-    $('.main-header:not(.sticky) .contact').waypoint(function(direction) {
-      var main_header = $(this).parents('.main-header');
-      var sticky_header = $('.main-header.sticky');
-
-      if ( direction === 'up' ) {
-        sticky_header.hide();
-      } else if ( direction === 'down' ) {
-        sticky_header.show();
-      }
-    }, {
-      offset: 2,
-      horizontal: false
-    });
   });
 
   $('a.email').on('click', function () {
